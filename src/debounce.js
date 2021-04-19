898e61b0c0def09006fc52245311983711bd05e9
zhaowenhao9 <zhaowenhao14@jd.com>
2021-04-19 15:45:03 +0800

    feat: debounce, throttle

---
+++ src/debounce.js
@@ -0,0 +1,16 @@
+/**
+ * @description 防抖
+ * @param {Function} func 执行函数
+ * @param {Number} delay 防抖时长
+ */
+
+let timer = null;
+export default function debounce(func, delay) {
+  if (timer) return;
+  if (timer === null) func(); // 第一次立即执行
+  timer = setTimeout(() => {
+    func()
+    timer = false;
+  }, Number(delay))
+}
+
---
+++ src/throttle.js
@@ -0,0 +1,18 @@
+/**
+ * @description 节流
+ * @param {Function} func 执行函数
+ * @param {Number} delay 节流时长
+ */
+
+let timer = null;
+function throttle(func, delay) {
+  if (timer) clearTimeout(timer)
+
+  timer = setTimeout(() => {
+    func()
+  }, Number(delay))
+}
+
+
+
+
