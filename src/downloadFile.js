7df077fc018a8dcaf404da4475e13d8a61a666e3
zhaowenhao14 <zhaowenhao14@jd.com>
2021-06-10 18:17:23 +0800

    feat: 完善文件下载函数

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -2,27 +2,28 @@ import axios from 'axios';
 
 /**
  * 文件下载工具函数
- * @param {string} fileUrl 文件链接
+ * @param {string} url 文件链接
  * @param {string} name 文件名称
+ * @param {string} type 文件类型
  *
  * URL.createObjectURL() 静态方法会创建一个 DOMString，其中包含一个表示参数中给出的对象的URL。
  * blob:http://localhost:3000/f76cc4c9-a2ac-4ac1-9e85-b9760be05206
  */
-export const downloadFile = async (fileUrl, name) => {
+export const downloadFile = async (name, url, type = '') => {
   try {
-    const res = await axios.get(fileUrl, { responseType: 'blob' });
-    const downloadUrl = window.URL.createObjectURL(
-      new Blob([res.data], { type: res.data.type })
-    );
+    const res = await axios.get(url, { responseType: 'blob' });
+    const downloadUrl = window.URL.createObjectURL(new Blob([res.data], { type: res.data.type }));
     const link = document.createElement('a');
     link.href = downloadUrl;
-    link.setAttribute('download', name);
+
+    type = type && type[0] !== '.' ? `.${type}` : type;
+    link.setAttribute('download', `${name}${type}`);
     document.body.appendChild(link);
     link.click();
     link.remove();
     window.URL.revokeObjectURL(downloadUrl);
   } catch (error) {
-    console.log(error);
+    message.error(error && error.message);
   }
 };
 
